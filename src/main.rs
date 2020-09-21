ce80233a1f56a1bdde27a8c3d0f95d1abf789cf3
pk1890 <piotrekkotara@gmail.com>
2020-09-21 22:55:34 +0200

    rewritten add_value to take u8 slice; changed pointers to atomic pointers in the buffer

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use core::mem::size_of;
+use core::sync::atomic::{AtomicPtr, Ordering};
 
 const BUFFER_SIZE: usize = 1024;
 
@@ -16,10 +17,10 @@ struct BufferEntryPreamble{
 
 #[repr(C)]
 struct CircullarBuffer{
-    data: [u64; BUFFER_SIZE],
-    read_pointer: u64,
-    write_pointer: u64,
-    release_pointer: u64,
+    data: [u8; BUFFER_SIZE],
+    read_pointer: AtomicPtr<u8>,
+    write_pointer: AtomicPtr<u8>,
+    release_pointer: AtomicPtr<u8>,
 }
 
 
@@ -27,69 +28,82 @@ impl CircullarBuffer{
     pub fn new() -> Self{
         let mut buff = CircullarBuffer{
             data: [0;BUFFER_SIZE],
-            read_pointer: 0,
-            write_pointer: 0,
-            release_pointer: 0
+            read_pointer: AtomicPtr::new(&mut 0),
+            write_pointer: AtomicPtr::new(&mut 0),
+            release_pointer: AtomicPtr::new(&mut 0),
         };
         
-        let address = &buff as *const _ as u64;
-        buff.read_pointer = address;
-        buff.write_pointer = address;
-        buff.release_pointer = address;
+        let address = &mut buff as *mut _ as *mut u8;
+        buff.read_pointer.store(address, Ordering::Relaxed);
+        buff.write_pointer.store(address, Ordering::Relaxed);
+        buff.release_pointer.store(address, Ordering::Relaxed);
 
-        println!("{:#018x}", address);
+        println!("{:#018x}", address as u64);
 
         buff
     }
 
-    fn add_val<T>(&mut self, value: T, entry_type: u64){
-        unsafe{
-            let preamble = BufferEntryPreamble{
-                entry_type: entry_type,
-                size: size_of::<T>() as u64,
-            };
+    // fn add_val<T>(&mut self, value: T, entry_type: u64){
+    //     unsafe{
+    //         let preamble = BufferEntryPreamble{
+    //             entry_type: entry_type,
+    //             size: size_of::<T>() as u64,
+    //         };
             
-            unsafe {
-                core::ptr::copy_nonoverlapping(
-                    &preamble as *const BufferEntryPreamble,
-                    self.write_pointer as *mut BufferEntryPreamble,
-                    1,
-                );
+    //         unsafe {
+    //             let ptr = 
+    //             core::ptr::copy_nonoverlapping(
+    //                 &preamble as *const BufferEntryPreamble,
+    //                 self.write_pointer.load(Ordering::Acquire) as *mut BufferEntryPreamble,
+    //                 1,
+    //             );
 
-                self.write_pointer += size_of::<BufferEntryPreamble>() as u64;
+    //             self.write_pointer. += size_of::<BufferEntryPreamble>() as u64;
 
-                core::ptr::copy_nonoverlapping(
-                    &value as *const T,
-                    self.write_pointer as *mut T,
-                    1,
-                );
+    //             core::ptr::copy_nonoverlapping(
+    //                 &value as *const T,
+    //                 self.write_pointer as *mut T,
+    //                 1,
+    //             );
                 
-                self.write_pointer += size_of::<T>() as u64;
+    //             self.write_pointer += size_of::<T>() as u64;
 
-            }
-        }
-    }
+    //         }
+    //     }
+    // }
 
-    fn read_value<T>(&mut self) -> T{
-        let mut res: T;
+    // fn read_value<T>(&mut self) -> T{
+    //     let mut res: T;
 
+    //     unsafe{
+    //         self.read_pointer += size_of::<BufferEntryPreamble>() as u64;
+    //         core::ptr::copy_nonoverlapping(
+    //             self.read_pointer as *const T,
+    //             &mut res as *mut T,
+    //             1
+    //         );
+    //     }
+
+    //     res
+
+    // }
+
+    pub fn add_value(&mut self, data: &[u8]){
         unsafe{
-            self.read_pointer += size_of::<BufferEntryPreamble>() as u64;
+            let mut pointer = self.write_pointer.load(Ordering::Acquire);
             core::ptr::copy_nonoverlapping(
-                self.read_pointer as *const T,
-                &mut res as *mut T,
+                &data.len() as *const _ as *const u64,
+                pointer as *mut u64,
                 1
             );
-        }
-
-        res
-
-    }
-
-    pub fn add_value(&mut self, entry: BufferEntry){
-        match entry{
-            BufferEntry::Simple(val) => self.add_val(val, 0),
-            BufferEntry::Double(val) => self.add_val(val, 1),
+            pointer = pointer.add(8); //add u64
+            core::ptr::copy_nonoverlapping(
+                data as *const _ as *const u8,
+                pointer,
+                data.len()
+            );
+            pointer = pointer.add(data.len());
+            self.write_pointer.store(pointer, Ordering::Release);
         }
     }
 }
@@ -99,12 +113,12 @@ fn main() {
     println!("Hello, world!");
     let mut buff = CircullarBuffer::new();
 
-    buff.add_value(BufferEntry::Simple(420));
-    buff.add_value(BufferEntry::Double([21, 37]));
-    buff.add_value(BufferEntry::Simple(69));
+    buff.add_value(&[69]);
+    buff.add_value(&[21, 37]);
+    buff.add_value(&[23, 12, 32, 12]);
 
-    for i in 0..10{
-        println!("{}", buff.data[i]);
+    for i in 0..30{
+        println!("{}: {}", i, buff.data[i]);
     }
 }
 
