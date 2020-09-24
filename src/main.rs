bcec1cc649959c43d12c1dffe1887f0926d1b537
pk1890 <piotrekkotara@gmail.com>
2020-09-24 22:26:20 +0200

    completer reservation and declaration mechanism for non-circullar buffer

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,40 @@
 use core::mem::size_of;
 use core::sync::atomic::{AtomicPtr, Ordering};
 
-const BUFFER_SIZE: usize = 1024;
+const BUFFER_SIZE: usize = 100;
 
 // #[repr(u64)]
-enum BufferEntry{
-    Simple(u64),
-    Double([u64;2])
+#[derive(Debug)]
+enum BufferAddValueError{
+    SizeTooBig
 }
 
+
 #[repr(C)]
-struct BufferEntryPreamble{
-    entry_type: u64,
-    size: u64
+struct ReservedMemory<'a>{
+    memory: &'a mut [u8],
+    buffer: &'a CircullarBuffer,
+}
+
+impl<'a> core::ops::Deref for ReservedMemory<'a>{
+    type Target = [u8];
+
+    fn deref(&self) -> &Self::Target {
+        &(*self.memory)
+    }
+}
+
+
+impl<'a> core::ops::DerefMut for ReservedMemory<'a>{
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut(*self.memory)
+    }
+}
+
+impl<'a> Drop for ReservedMemory<'a>{
+    fn drop(&mut self) {
+        self.buffer.declare();
+    }
 }
 
 #[repr(C)]
@@ -21,6 +43,7 @@ struct CircullarBuffer{
     read_pointer: AtomicPtr<u8>,
     write_pointer: AtomicPtr<u8>,
     release_pointer: AtomicPtr<u8>,
+    reservation_pointer: AtomicPtr<u8>,
 }
 
 
@@ -31,118 +54,108 @@ impl CircullarBuffer{
             read_pointer: AtomicPtr::new(&mut 0),
             write_pointer: AtomicPtr::new(&mut 0),
             release_pointer: AtomicPtr::new(&mut 0),
+            reservation_pointer: AtomicPtr::new(&mut 0),
         };
         
         let address = &mut buff as *mut _ as *mut u8;
         buff.read_pointer.store(address, Ordering::Relaxed);
         buff.write_pointer.store(address, Ordering::Relaxed);
         buff.release_pointer.store(address, Ordering::Relaxed);
+        buff.reservation_pointer.store(address, Ordering::Relaxed);
 
         println!("{:#018x}", address as u64);
 
         buff
     }
 
-    // fn add_val<T>(&mut self, value: T, entry_type: u64){
-    //     unsafe{
-    //         let preamble = BufferEntryPreamble{
-    //             entry_type: entry_type,
-    //             size: size_of::<T>() as u64,
-    //         };
-            
-    //         unsafe {
-    //             let ptr = 
-    //             core::ptr::copy_nonoverlapping(
-    //                 &preamble as *const BufferEntryPreamble,
-    //                 self.write_pointer.load(Ordering::Acquire) as *mut BufferEntryPreamble,
-    //                 1,
-    //             );
 
-    //             self.write_pointer. += size_of::<BufferEntryPreamble>() as u64;
-
-    //             core::ptr::copy_nonoverlapping(
-    //                 &value as *const T,
-    //                 self.write_pointer as *mut T,
-    //                 1,
-    //             );
-                
-    //             self.write_pointer += size_of::<T>() as u64;
-
-    //         }
-    //     }
-    // }
-
-    // fn read_value<T>(&mut self) -> T{
-    //     let mut res: T;
-
-    //     unsafe{
-    //         self.read_pointer += size_of::<BufferEntryPreamble>() as u64;
-    //         core::ptr::copy_nonoverlapping(
-    //             self.read_pointer as *const T,
-    //             &mut res as *mut T,
-    //             1
-    //         );
-    //     }
-
-    //     res
-
-    // }
-
-    pub fn reserve(&mut self, size: usize) -> &mut [u8]{
+    pub fn reserve(&self, size: usize) -> Result<ReservedMemory, BufferAddValueError>{
+        if size >> core::mem::size_of::<usize>()*8-1 != 0 {
+            return Err(BufferAddValueError::SizeTooBig);
+        }
         unsafe{
-            let mut pointer = self.write_pointer.load(Ordering::Acquire);
+            let mut pointer = self.reservation_pointer.load(Ordering::Acquire);
             let alignment = core::mem::align_of::<usize>();
             let modulo = (pointer as usize ) % alignment; 
             if modulo != 0 {
                 pointer = pointer.add(alignment - ( (pointer as usize ) % alignment)); // Align pointer to usize alignment
             }
+
+            let end_of_buffer = self as *const _ as usize + BUFFER_SIZE;
+            if pointer as usize > end_of_buffer || end_of_buffer - (pointer as usize) - core::mem::size_of::<usize>() < size{
+                return Err(BufferAddValueError::SizeTooBig);
+            }
+
             core::ptr::copy_nonoverlapping(
                 &size as *const usize,
                 pointer as *mut usize,
                 1
             );
             pointer = pointer.add(size_of::<usize>()); //set pointer after inserted usize
-            core::slice::from_raw_parts_mut::<u8>(pointer, size)
+            let res = Ok(ReservedMemory{
+                memory: core::slice::from_raw_parts_mut::<u8>(pointer, size),
+                buffer: self,
+            });
+            self.reservation_pointer.store(pointer.add(size), Ordering::Release);
+            res
         }
-        
     }
 
-    pub fn declare(&mut self){
+    fn declare(&self){
+        if self.reservation_pointer.load(Ordering::Acquire) as u64 == self.write_pointer.load(Ordering::Acquire) as u64{
+            return;
+        }
+        let only_msb_of_usize = 1 << core::mem::size_of::<usize>()*8-1;
+        let mut pointer = self.write_pointer.load(Ordering::Acquire);
+        let alignment = core::mem::align_of::<usize>();
+        let modulo = (pointer as usize ) % alignment; 
+        let difference = if modulo == 0 {0} else {alignment - ( (pointer as usize ) % alignment)}; // Align pointer to usize alignment
         unsafe{
-            let pointer = self.write_pointer.load(Ordering::Acquire);
-            let alignment = core::mem::align_of::<usize>();
-            let modulo = (pointer as usize ) % alignment; 
-            let difference = if modulo == 0 {0} else {alignment - ( (pointer as usize ) % alignment)}; // Align pointer to usize alignment
-            let size = *(pointer.add(difference) as *const usize);
-            println!("{}", size);
-            self.write_pointer.store(pointer.add(difference+core::mem::size_of::<usize>()+size), Ordering::Release);
-            println!("{:#018x}", self.write_pointer.load(Ordering::Acquire) as usize);
+            let size = &mut (*((pointer.add(difference) as *mut usize)));
+            let size_value = *size & !only_msb_of_usize;
+
+            pointer = pointer.add(difference+core::mem::size_of::<usize>()+size_value);
+            *size |= only_msb_of_usize;
+            loop{
+                let size = *(pointer.add(difference) as *const usize) & !only_msb_of_usize;
+                let difference = if modulo == 0 {0} else {alignment - ( (pointer as usize ) % alignment)}; // Align pointer to usize alignment
+                
+                if size >> core::mem::size_of::<usize>()*8-1 == 0{
+                    self.write_pointer.store(pointer, Ordering::Release);
+                    return;
+                }
+                pointer = pointer.add(difference+core::mem::size_of::<usize>()+size);
+            }
+
         }
     }
+
 }
 
 
 fn main() {
     println!("Hello, world!");
-    let mut buff = CircullarBuffer::new();
+    let buff = CircullarBuffer::new();
+    {
 
-    let a = buff.reserve(3);
-    a[0] = 4;
-    a[1] = 15;
-    a[2] = 100;
+        let mut a = buff.reserve(3).expect("No i za duze");
+        a[0] = 4;
+        a[1] = 15;
+        a[2] = 100;
+        let addr = &buff.data as *const _ as usize; 
+        for i in 0..BUFFER_SIZE{
+            println!("({}){:#018x}: {}", i, addr+i, buff.data[i]);
+        }
+        println!("SECOND ROUND");
+        let mut b = buff.reserve(2).expect("No i za duze");
+        b[0] = 69;
+        b[1] = 88;
+
+    }
     let addr = &buff.data as *const _ as usize; 
-    for i in 0..30{
+    for i in 0..BUFFER_SIZE{
         println!("({}){:#018x}: {}", i, addr+i, buff.data[i]);
     }
-    println!("SECOND ROUND");
-    buff.declare();
-    let b = buff.reserve(2);
-    b[0] = 69;
-    b[1] = 88;
 
-    let addr = &buff.data as *const _ as usize; 
-    for i in 0..30{
-        println!("({}){:#018x}: {}", i, addr+i, buff.data[i]);
-    }
 }
 
